b64b9227d6d36c0f8314f961623537cfdeb7e3af
Mishaal Alanzi <micharadi@Mishaals-iMac.local>
2017-07-29 23:42:34 +0300

    Pagination

--- posts/views.py
+++ posts/views.py
@@ -4,6 +4,7 @@ from .models import Post
 from django.shortcuts import get_object_or_404
 from .forms import PostForm
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
@@ -21,8 +22,21 @@ def post_create(request):
 
 def post_list(request):
 	object_list = Post.objects.all()#.order_by("-timestamp", "-updated")
+
+	paginator = Paginator(object_list, 10) # Show 25 contacts per page
+
+	page = request.GET.get('page')
+	try:
+		objects = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		objects = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		objects = paginator.page(paginator.num_pages)
+
 	context = {
-		"the_object_list": object_list,
+		"the_object_list": objects,
 		"title": "List",		
 	}
 	return render(request, "post_list.html", context)
