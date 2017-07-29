1dcd3b18e052d632e07c37f09d1904d5807a0ed8
Mishaal Alanzi <micharadi@Mishaals-iMac.local>
2017-07-29 23:32:20 +0300

    QuerySet ordering

--- posts/models.py
+++ posts/models.py
@@ -16,5 +16,8 @@ class Post(models.Model):
 	def get_absolute_url(self):
 		return reverse("posts:detail", kwargs={"post_id": self.id})
 
+	class Meta:
+		ordering = ["-timestamp", "-updated"]
+
 
 	
